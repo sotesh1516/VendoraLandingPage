ecd26c667fb5f6a4f99958ff050d37495d26410c
sof1516 <teshomesofoniyas152@gmail.com>
2025-07-22 16:14:16 -0400

    fixed more cors bug

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://vendoralandingpage.onrender.com",
+    origin: "https://vendoralandingpage-1.onrender.com",
     credentials: true // If you're sending cookies or auth headers
   }));
 
