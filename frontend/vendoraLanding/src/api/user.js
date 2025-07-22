eff82d6c84a1a3aa77fbdfccaea551f2a366a0e1
Sofoniyas Alemayehu <108954304+sotesh1516@users.noreply.github.com>
2025-07-22 16:03:15 -0400

    Update user.js

--- frontend/vendoraLanding/src/api/user.js
+++ frontend/vendoraLanding/src/api/user.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const signUpUser = async (userInfo) => {
-    const response = await axios.post("http://localhost:3000/user", {
+    const response = await axios.post("https://vendoralandingpage.onrender.com", {
         userEmail: userInfo.userEmail,
     });
 
